4050d9d3cd36917521cefbcaaf84bbfb54c035e7
oscarfu_0501 <59132665+Unabashed0501@users.noreply.github.com>
2023-10-21 16:04:41 +0800

    :sparkles: add note and message pages

---
+++ gesture_memorize/lib/Components/Text/big_text.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class BigText extends StatelessWidget {
+  final String text;
+  final double size;
+  final Color fontColor;
+  final FontWeight fontWeight;
+
+  const BigText({
+    super.key,
+    required this.text,
+    this.size = 30.0,
+    this.fontColor = const Color.fromARGB(255, 0, 0, 0),
+    this.fontWeight = FontWeight.bold,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      maxLines: 1,
+      overflow: TextOverflow.ellipsis,
+      text,
+      style: TextStyle(
+        fontSize: size,
+        fontWeight: fontWeight,
+        fontFamily: "Poppins",
+        color: fontColor,
+      ),
+    );
+  }
+}
---
+++ gesture_memorize/lib/Infomations/message_list_info.dart
@@ -0,0 +1,83 @@
+class MessageListInfo {
+  MessageListInfo._();
+  static List<Map<String, dynamic>> recentpeople = [
+    {
+      "name": "Dienrui",
+      "image": 'assets/images/image01.jpg'
+    },
+    {
+      "name": "Walker",
+      "image": 'assets/images/image02.jpg'
+    },
+    {
+      "name": "Oscar",
+      "image": 'assets/images/image03.jpg'
+    },
+    {
+      "name": "Mark",
+      "image": 'assets/images/image04.jpg'
+    },
+    {
+      "name": "Jlin",
+      "image":'assets/images/winnie.jpg'
+    },
+  ];
+  static List<Map<String, dynamic>> people = [
+    {
+      "name": 'snoopy',
+      "image": 'assets/images/snoopy.png',
+      "date": '08:43',
+      "gmail": 'snoopy@gmail.com'
+    }, 
+    {
+      "name": 'Beautiful girl',
+      "image": 'assets/images/beautiful_girl.jpg',
+      "date": 'Tue',
+      "gmail": "I'm so hot",
+    }, 
+    {
+      "name": "William",
+      "image": 'assets/images/handsome_boy.jpg',
+      "date": '23 Mar',
+      "gmail": "I'm so handsome",
+    },
+    {
+      "name": "rabbit",
+      "image": 'assets/images/rabbit.jpg',
+      "date": 'Wed',
+      "gmail": 'rabbit@gmail.com',
+    },
+    {
+      "name": "duck",
+      "image": 'assets/images/duck.jpg',
+      "date": '08:43',
+      "gmail": 'ddddddduck',
+    },
+    {
+      "name": "stitch",
+      "image": 'assets/images/stitch.jpg',
+      "date": '08:43',
+      "gmail": 'sss.. sss ...',
+    },
+    {
+      "name": "stitch",
+      "image": 'assets/images/stitch.jpg',
+      "date": '08:43',
+      "gmail": 'sss.. sss ...',
+    },
+    {
+      "name": "stitch",
+      "image": 'assets/images/stitch.jpg',
+      "date": '08:43',
+      "gmail": 'sss.. sss ...',
+    },
+    {
+      "name": "stitch",
+      "image": 'assets/images/stitch.jpg',
+      "date": '08:43',
+      "gmail": 'sss.. sss ...',
+    },
+  ];
+}
+
+
